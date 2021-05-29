cf9f963bf5315365f947d6fc5b29b8af6747183f
Zain Afzal <zain.afzal@rise.cx>
2021-05-29 21:27:54 +1000

    Fixing - Not inserting multiple item yet

--- backend/scrumblr-api/api/app.js
+++ backend/scrumblr-api/api/app.js
@@ -3,19 +3,16 @@ const app = express();
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
-AWS.config.update({
-    endpoint:'http://docker.for.mac.localhost:4566',
-});
-
-const docClient = new AWS.DynamoDB.DocumentClient({ 
+const docClient = new AWS.DynamoDB.DocumentClient({
+    region: 'ap-southeast-2',
     endpoint:'http://docker.for.mac.localhost:4566' 
 });
 
-const table = "Board";                         // replace with board name coming from the frontend
+const table = "DevCop";                         // replace with board name coming from the frontend
 
 app.get("/", async(req, res) => {
 
-    var params = {
+    let params = {
         TableName: table,
     };
     
@@ -25,23 +22,61 @@ app.get("/", async(req, res) => {
 
 
 app.post("/", async(req, res) => {
+ 
+    //et textForNotes = ['zero', 'one', 'two', 'three'];
 
-    var params = {
-        TableName: table,
-        Item : {
-            uuid : uuidv4(),
-            datetime: '26/02/2021',              // replace with date and time object
-            board_name: "devcop",                // replace with board name in the url path (coming from the frontend)
-            note1 : "This is my test note 6"     // replace with note(text) coming from the frontend
+    // prepare notes for insertion in dynamodb
+    //for (let i = 0; i < textForNotes.length; i++){
+        
+        let params = {
+            RequestItems:{
+                "DevCop": [
+                    {
+                        "PutRequest": {
+                            "Item": {
+                                "board_id": {
+                                    "S": uuidv4().toString
+                                },
+                                "note_id": {
+                                    "S": uuidv4().toString
+                                }
+                            }
+                        }
+                     },
+                     {
+                        "PutRequest": {
+                            "Item": {
+                                "board_id": {
+                                    "S": uuidv4().toString
+                                },
+                                "note_id": {
+                                    "S": uuidv4().toString
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+    };
+        
+        // TableName: table,
+        // Item : {
+        //     BoardId : uuidv4(),
+        //     NoteId: uuidv4(),
+        //     DateTime: '12/12/12', // replace with date and time object
+        //     NoteText : textForNotes[i]  // replace with note(text) coming from the frontend
+        // }
+    
+        await docClient.batchWrite(params, function (err, data){
+            console.log(data)
+        if (err) {
+            res.status(500).send('An error occurred -> ' + err);
+        } else {
+            res.status(201).send('Inserted successfully ' +  data);
         }
-    }
-    await docClient.put(params, function (err, data){
-    if (err) {
-        res.status(201).send('An error occurred -> ' + err);
-      } else {
-        res.status(201).send('Inserted successfully');
-      }
+    
     }).promise();
+    
 });
 
 
