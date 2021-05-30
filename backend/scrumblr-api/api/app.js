32dc2c87247dbec53379c1ca37ddda78e52367c4
Zain Afzal <zain.afzal@rise.cx>
2021-05-30 14:32:13 +1000

    insert data in memory first

--- backend/scrumblr-api/api/app.js
+++ backend/scrumblr-api/api/app.js
@@ -1,85 +1,44 @@
-const express = require('express');
-const app = express();
-const AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid');
+const express = require('express')
+const app = express()
+const router = express.Router()
+const bodyParser = require('body-parser')
+const LorenIpsum = require('lorem-Ipsum').loremIpsum
+const port = 3000
+const { uuid } = require('uuidv4')
 
-const docClient = new AWS.DynamoDB.DocumentClient({
-    region: 'ap-southeast-2',
-    endpoint:'http://docker.for.mac.localhost:4566' 
-});
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({extended: true}))
 
-const table = "DevCop";                         // replace with board name coming from the frontend
+let devcop = []
 
-app.get("/", async(req, res) => {
-
-    let params = {
-        TableName: table,
-    };
-    
-    const data = await docClient.scan(params).promise();
-    res.send(JSON.stringify(data));
-});
-
-
-app.post("/", async(req, res) => {
- 
-    //et textForNotes = ['zero', 'one', 'two', 'three'];
-
-    // prepare notes for insertion in dynamodb
-    //for (let i = 0; i < textForNotes.length; i++){
-        
-        let params = {
-            RequestItems:{
-                "DevCop": [
-                    {
-                        "PutRequest": {
-                            "Item": {
-                                "board_id": {
-                                    "S": uuidv4().toString
-                                },
-                                "note_id": {
-                                    "S": uuidv4().toString
-                                }
-                            }
-                        }
-                     },
-                     {
-                        "PutRequest": {
-                            "Item": {
-                                "board_id": {
-                                    "S": uuidv4().toString
-                                },
-                                "note_id": {
-                                    "S": uuidv4().toString
-                                }
-                            }
-                        }
-                    }
-                ]
+//Create a board with 3 sample notes
+router.get("/", function(req, res){
+    let board = {
+        board_id: uuid(),
+        notes: [
+            {
+                "text": new LorenIpsum(),
+                "location":uuid(),
+                "createdAt": Date.now()
+            },
+            {
+                "text":new LorenIpsum(),
+                "location":uuid(),
+                "createdAt": Date.now()
+            },
+            {
+                "text":new LorenIpsum(),
+                "location":uuid(),
+                "createdAt": Date.now()
             }
-    };
-        
-        // TableName: table,
-        // Item : {
-        //     BoardId : uuidv4(),
-        //     NoteId: uuidv4(),
-        //     DateTime: '12/12/12', // replace with date and time object
-        //     NoteText : textForNotes[i]  // replace with note(text) coming from the frontend
-        // }
-    
-        await docClient.batchWrite(params, function (err, data){
-            console.log(data)
-        if (err) {
-            res.status(500).send('An error occurred -> ' + err);
-        } else {
-            res.status(201).send('Inserted successfully ' +  data);
-        }
-    
-    }).promise();
-    
-});
+        ]
+    }
+    devcop.push(board);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(devcop));
+})
 
-
-app.listen(3000);
-  
-module.exports = app;
+//List all boards in the memory
+router.get("/board", async(req, res) => {
+res.send(JSON.stringify(devcop))
+})
