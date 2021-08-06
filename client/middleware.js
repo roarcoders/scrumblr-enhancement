f9309b87d60fe063a9a49624d63b9f37946e8bc1
Thanesh Pannirselvam <t.pannirselvam@fdmgroup.com>
2021-08-06 21:21:04 +1000

    padding working post board name

--- client/middleware.js
+++ client/middleware.js
@@ -1,70 +1,73 @@
-window.onload = localStorage.getItem("boardName");
-
-//Append board name to url after page loaded.
-window.history.replaceState(null, null, localStorage.getItem("boardName"))
-let url = 'https://we7btpj0ue.execute-api.ap-southeast-2.amazonaws.com/prod/board'
+let url =
+  "https://we7btpj0ue.execute-api.ap-southeast-2.amazonaws.com/prod/board";
 
 function go() {
-    console.log("go")
-    var value = document.forms[0].elements["name"].value;
-    value = escape(value);
-    //Load index.html after name entered and go button clicked.
-    //window.location.href = "index.html";
-    localStorage.setItem("boardName", value);
-    return false;
+  var value = document.forms[0].elements["name"].value;
+  value = escape(value);
+
+  localStorage.setItem("boardName", value);
+
+  //Post board name to backend.
+  postBoardName(value);
+
+  //Load index.html after name entered and go button clicked.
+  // window.location.href = "index.html";
+
+  //Append board name to url.sl
+  // window.history.replaceState(null, null, value);
+
+  return false;
 }
 
+async function postBoardName(boardName) {
+  // let board_name = document.forms[0].elements["name"].value;
 
-function postBoardName(){
-    let board_name = document.forms[0].elements["name"].value;
-    let board = {
-        'BoardName' : board_name
-    }
+  // let board = {
+  //   BoardName: board_name,
+  // };
 
-    //  let _data = {
-        
-    //      body:board
-    // //     //window.location.pathname
-    // }
-//    var header = new Header();
-//    header.append('Content-type','application/json; charset=UTF-8');
-//    header.append('Access-Control-Allow-Origin' , '*');
+  //  let _data = {
 
-alert(typeof(board));
-alert(board.BoardName);
- 
-    fetch(url, {
-        method: 'POST',
-       body: {
-           'BoardName' : board_name
-       },
-        headers: {
-            'Content-type':'application/json',
-        'Access-Control-Allow-Origin' : '*'
-                },
-                mode: 'no-cors' 
-    }
-    )
-   
-    // _parseJSON: function(response) { 
+  //      body:board
+  // //     //window.location.pathname
+  // }
+  //    var header = new Header();
+  //    header.append('Content-type','application/json; charset=UTF-8');
+  //    header.append('Access-Control-Allow-Origin' , '*');
+
+  //   alert(typeof board);
+  //   alert(board.BoardName);
+
+  await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      BoardName: boardName,
+    }),
+  })
+    // _parseJSON: function(response) {
     //     return response.text()
-    //     .then(function(text) 
+    //     .then(function(text)
     //     { return text ? JSON.parse(text) : {} }) }
 
+    .then((response) => {
+      window.location.href = "index.html";
 
-    .then(response => { 
-        return response.text() 
-        .then(function(text) 
-        { return text ? JSON.parse(text) : {} }) } )
-    .catch(err => console.log(err));
-   
+      return response.text().then(function (text) {
+        return text ? JSON.parse(text) : {};
+      });
+    })
+    .catch((err) => console.log(err));
 }
 
 function getBoards() {
-
-
-    // fetch(url)
-    //     .then(response => response.text())
-    //     .then(json => {console.log(JSON.stringify(json))})
-
+  fetch(url)
+    .then((response) => response.text())
+    .then((json) => {
+      console.log(JSON.stringify(json));
+    });
 }
