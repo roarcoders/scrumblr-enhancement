4a88d7f218e448369284bb4946bac699b8b74a08
Kartik Kumar <s3788894@student.rmit.edu.au>
2021-09-10 23:19:01 +1000

    implemented all middleware functionalities

--- client/middleware.js
+++ client/middleware.js
@@ -1,6 +1,5 @@
 let url =
-  "https://kyp91ncs12.execute-api.ap-southeast-2.amazonaws.com/prod/board";
-
+"https://boy22uup5e.execute-api.ap-southeast-2.amazonaws.com/prod/board/"
 let middlware_boardid = "";
 function go() {
   
@@ -8,21 +7,32 @@ function go() {
   value = escape(value);
 
   localStorage.setItem("boardName", value);
+// deleteBoard("09e49698-05b6-4457-8271-2a288af9f6f5")
+// getBoardById("6f28a5d4-b14c-455b-9245-60d9e561d84e");
+// getBoards();
+// let responseCode = postBoardName(value);
 
-  let responseCode = postBoardName(value);
-  console.log("response code :"+JSON.stringify(responseCode));
-  // getBoards();
+
+
+
+
+// postNote("I am a note", "6f28a5d4-b14c-455b-9245-60d9e561d84e");
+// getNote("6f28a5d4-b14c-455b-9245-60d9e561d84e","5f216c4d-4aef-42c1-8fc3-0a1c4e076650")
+// patchNote("6f28a5d4-b14c-455b-9245-60d9e561d84e","5f216c4d-4aef-42c1-8fc3-0a1c4e076650","I am a new note now, yayyy!!");
+//deleteNote("6f28a5d4-b14c-455b-9245-60d9e561d84e", "794166f2-bd7f-4001-84e8-4ec2fac0c0ca")
+
+  // console.log("response code :"+JSON.stringify(responseCode));
   //Delaying code run for 500ms so that postBoardName is able to penetrate the request
   sleep(500);
   
   // Post board name to backend.
-  if(responseCode === 200)
-   {
-        window.location.href = "index.html";
+  // if(responseCode === 200)
+  //  {
+  //       window.location.href = "index.html";
     
-        //Append board name to url.sl
-       window.history.replaceState(null, null, value);
-  }
+  //       //Append board name to url.sl
+  //      window.history.replaceState(null, null, value);
+  // }
   //Uncomment or comment when testing
   // console.log(getBoardByName(value));
 
@@ -96,4 +106,153 @@ function getBoards() {
     while (new Date().getTime() < start + delay);
 }
 
+async function postNote(value,boardIdtopost)
+{
+  let noteName = value;
+ await fetch(url+boardIdtopost+"/note/", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      singleNote: noteName,
+    }),
+  })
 
+  .then((response) => {
+        return response.text().then(function (text) {
+     
+      return text ? JSON.parse(text) : {
+        
+        
+      };
+    });
+  })
+console.log("note inserted");
+}
+
+async function getBoardById(boardIdtoGet)
+{
+ let  board = await fetch (url+boardIdtoGet,
+  {
+    method : "GET",
+    mode : "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+  })
+
+  .then((response) => {
+    //window.location.href = "index.html";
+    // middlware_boardid=JSON.stringify(response.JSON());
+
+    return response.text().then(function (text) {
+     
+      return text ? JSON.parse(text) : {
+        
+        
+      };
+    });
+  })
+  console.log(board);
+}
+
+async function getNote(boardId,noteId)
+{
+  let  note = await fetch (url+boardId+"/note/"+noteId,
+    {
+      method : "GET",
+      mode : "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+    })
+  
+    .then((response) => {
+      //window.location.href = "index.html";
+      // middlware_boardid=JSON.stringify(response.JSON());
+  
+      return response.text().then(function (text) {
+       
+        return text ? JSON.parse(text) : {
+          
+          
+        };
+      });
+    })
+    console.log(note);
+  }
+
+  async function patchNote(boardId,noteId,newNote)
+{
+await fetch (url+boardId+"/note/"+noteId,
+    {
+      method : "PATCH",
+      mode : "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "*"
+        },
+        body: JSON.stringify({
+          singleNote: newNote,
+        })
+
+    })
+  
+    .then((response) => {
+      //window.location.href = "index.html";
+      // middlware_boardid=JSON.stringify(response.JSON());
+  
+      return response.text().then(function (text) {
+       
+        return text ? JSON.parse(text) : {
+          
+          
+        };
+      });
+    })
+    
+  }
+
+  async function deleteBoard(boardId)
+  {
+   let message =  await fetch (url+boardId,
+    {
+      method : "DELETE",
+      mode : "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+    })
+    .then((response) => response.text())
+    .then((json) => {
+      console.log(JSON.stringify(json));
+    });
+
+
+  }
+
+  async function deleteNote(boardId,noteId)
+  {
+    let message = await fetch (url+boardId+"/note/"+noteId,
+    {
+      method : "DELETE",
+      mode: "cors",
+      headers : 
+      {
+        "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+      }
+    })
+    .then((response) => response.text())
+    .then((json) => {
+      console.log(JSON.stringify(json));
+    });
+
+  }
